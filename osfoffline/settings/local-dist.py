43734dcc20502695e7508c137f33d2d80c8d482a
himanshuo <https://himanshuo@github.com>
2015-11-20 16:11:02 -0500

    undo url rename

--- osfoffline/settings/local-dist.py
+++ osfoffline/settings/local-dist.py
@@ -3,5 +3,5 @@ POLL_DELAY = 5  # seconds
 
 LOG_LEVEL = 'DEBUG'
 
-API_BASE = 'http://www.osf-api.io'
+API_BASE = 'http://localhost:8000'
 FILE_BASE = 'http://localhost:7777'  # FIXME: Dev mode currently does not work with local waterbutler (abought)
